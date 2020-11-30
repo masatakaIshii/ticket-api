230e111df9760f1e1c1f253d2467f758429b344b
MasatakaIshii <masaradio78@gmail.com>
2020-11-30 22:15:15 +0100

    First commit

---
+++ src/main/java/fr/esgi/ticketapi/TicketApiApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.ticketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketApiApplication.class, args);
+	}
+
+}
