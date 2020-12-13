9221a6f62a6bc493f14c81403bb3431e209d96a4
MasatakaIshii <masaradio78@gmail.com>
2020-12-13 23:18:19 +0100

    classes of database tables done

--- src/main/java/fr/esgi/ticketapi/dataprovider/model/TOrderState.java
+++ src/main/java/fr/esgi/ticketapi/dataprovider/model/TOrderState.java
@@ -1,4 +1,51 @@
 package fr.esgi.ticketapi.dataprovider.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class TOrderState {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    private TOrder order;
+
+    @ManyToOne
+    private TState state;
+
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public TOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(TOrder order) {
+        this.order = order;
+    }
+
+    public TState getState() {
+        return state;
+    }
+
+    public void setState(TState state) {
+        this.state = state;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
--- src/main/java/fr/esgi/ticketapi/dataprovider/model/TState.java
+++ src/main/java/fr/esgi/ticketapi/dataprovider/model/TState.java
@@ -1,4 +1,41 @@
 package fr.esgi.ticketapi.dataprovider.model;
 
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
 public class TState {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "state")
+    private Collection<TOrderState> ordersStates;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<TOrderState> getOrdersStates() {
+        return ordersStates;
+    }
+
+    public void setOrdersStates(Collection<TOrderState> ordersStates) {
+        this.ordersStates = ordersStates;
+    }
 }
