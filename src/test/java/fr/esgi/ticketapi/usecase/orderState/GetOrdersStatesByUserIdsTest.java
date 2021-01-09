799616da74f179a8a333ed81302b97087450b964
JBertho <james.bertho94@gmail.com>
2021-01-09 15:30:00 +0100

    add test for all order states by list of userId

---
+++ src/test/java/fr/esgi/ticketapi/usecase/orderState/GetOrdersStatesByUserIdsTest.java
@@ -0,0 +1,104 @@
+package fr.esgi.ticketapi.usecase.orderState;
+
+import fr.esgi.ticketapi.core.dao.OrderDao;
+import fr.esgi.ticketapi.core.dao.OrderStateDao;
+import fr.esgi.ticketapi.core.entity.Order;
+import fr.esgi.ticketapi.core.entity.OrderState;
+import fr.esgi.ticketapi.core.entity.UserOrderState;
+import fr.esgi.ticketapi.infrastructure.dataprovider.dao.MySQLOrderStateDao;
+import fr.esgi.ticketapi.infrastructure.dataprovider.repository.OrderStateRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class GetOrdersStatesByUserIdsTest {
+    GetOrdersStatsByUserIds getOrdersStatesByUserIds;
+
+    GetOrdersStatsByUserIds getOrdersStatesByUserIdsWithMockedRepo;
+
+
+    @Mock
+    OrderStateDao mockOrderStateDao;
+
+    OrderStateDao realOrderStateDao;
+
+    @Mock
+    OrderDao mockOrderDao;
+
+    @Mock
+    OrderStateRepository mockOrderStateRepository;
+
+    List<Integer> userIds;
+
+    List<Order> mockOrders;
+
+    List<fr.esgi.ticketapi.infrastructure.dataprovider.model.OrderState> mockOrderStates;
+
+    @BeforeEach
+    public void setup() {
+        realOrderStateDao = new MySQLOrderStateDao(mockOrderStateRepository);
+
+        getOrdersStatesByUserIds = new GetOrdersStatsByUserIds(mockOrderStateDao, mockOrderDao);
+        getOrdersStatesByUserIdsWithMockedRepo = new GetOrdersStatsByUserIds(realOrderStateDao, mockOrderDao);
+
+        userIds = new ArrayList<>();
+
+        mockOrders = new ArrayList<>();
+        mockOrders.add(new Order(1,2,123));
+        mockOrders.add(new Order(2,3,123));
+
+        Mockito.when(mockOrderDao.getOrdersByUserId(123)).thenReturn(mockOrders);
+
+        List<Integer> orderIds = new ArrayList<>();
+        orderIds.add(1);
+        orderIds.add(2);
+
+
+        mockOrderStates = new ArrayList<>();
+        mockOrderStates.add(new fr.esgi.ticketapi.infrastructure.dataprovider.model.OrderState(1,1,1, LocalDate.now().minusDays(2)));
+        mockOrderStates.add(new fr.esgi.ticketapi.infrastructure.dataprovider.model.OrderState(2,1,1, LocalDate.now().minusDays(1)));
+        mockOrderStates.add(new fr.esgi.ticketapi.infrastructure.dataprovider.model.OrderState(3,2,1, LocalDate.now()));
+
+
+        Mockito.when(mockOrderStateRepository.getAllByOrderIds(orderIds)).thenReturn(mockOrderStates);
+    }
+
+    @Test
+    public void should_call_orderDao_to_get_user_orders() {
+        userIds.add(123);
+        getOrdersStatesByUserIds.execute(userIds);
+        Mockito.verify(mockOrderDao, Mockito.times(userIds.size())).getOrdersByUserId(123);
+    }
+
+    @Test
+    public void should_call_orderStateDao_to_get_user_orders_states() {
+        userIds.add(123);
+        getOrdersStatesByUserIds.execute(userIds);
+
+        Mockito.verify(mockOrderStateDao, Mockito.times(userIds.size())).getStatesOfOrderIds(mockOrders.stream().map(Order::getId).collect(Collectors.toList()));
+    }
+
+    @Test
+    public void should_return_last_order_state_of_each_user_orders() {
+        userIds.add(123);
+        List<UserOrderState> result = getOrdersStatesByUserIdsWithMockedRepo.execute(userIds);
+        List<UserOrderState> expected = new ArrayList<>();
+        List<OrderState> userOrdersExpected = new ArrayList<>();
+        userOrdersExpected.add(new OrderState(1,1,1, LocalDate.now().minusDays(2)));
+        userOrdersExpected.add(new OrderState(2,1,1, LocalDate.now().minusDays(1)));
+        userOrdersExpected.add(new OrderState(3,2,1, LocalDate.now()));
+        expected.add(new UserOrderState(123,userOrdersExpected));
+
+        assertEquals(expected, result);
+    }
+}
