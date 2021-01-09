f955e6162e19f5e9b419fabea7890c7c47682d6a
JBertho <james.bertho94@gmail.com>
2021-01-09 15:37:28 +0100

    add order by userId

--- src/main/java/fr/esgi/ticketapi/infrastructure/entrypoints/rest/OrdersController.java
+++ src/main/java/fr/esgi/ticketapi/infrastructure/entrypoints/rest/OrdersController.java
@@ -2,7 +2,9 @@ package fr.esgi.ticketapi.infrastructure.entrypoints.rest;
 
 import fr.esgi.ticketapi.core.entity.Order;
 import fr.esgi.ticketapi.usecase.orders.GetOrders;
+import fr.esgi.ticketapi.usecase.orders.GetOrdersByUserId;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +15,21 @@ import java.util.List;
 public class OrdersController {
 
     private final GetOrders getOrders;
+    private final GetOrdersByUserId getOrdersByUserId;
 
-    public OrdersController(GetOrders getOrders) {
+    public OrdersController(GetOrders getOrders, GetOrdersByUserId getOrdersByUserId) {
         this.getOrders = getOrders;
+        this.getOrdersByUserId = getOrdersByUserId;
     }
 
     @GetMapping("")
     public List<Order> getAllOrders() {
+        return getOrders.execute();
+    }
+
+    @GetMapping("user/{id}")
+    public List<Order> getAllUserOrders(@PathVariable("id") Integer userId) {
+
         return getOrders.execute();
     }
 
